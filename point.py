8ec1b80fe994dcf539d8252a9c9212d242e85820
Reza-Amani <reza.amani@gmail.com>
2016-08-13 12:02:07 +1200

    bug/initialising water fixed

--- point.py
+++ point.py
@@ -3,11 +3,11 @@
     def __init__(self, X, Y):
         self.x = X 
         self.y = Y
-        if X >= 1300:
+        if X >= 1100:
             self.water = 4
-        elif X >= 1100:
-            self.water = 3
         elif X >= 800:
+            self.water = 3
+        elif X >= 600:
             self.water = 2
         else :
             self.water = 1
