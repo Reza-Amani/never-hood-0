2ce092ed41059d8a29144334f0920a81b2c53d4b
Reza-Amani <reza.amani@gmail.com>
2017-02-11 13:08:38 +1300

    bitmap and coloring the pixel is tested

--- NeverLand1/NeverLand1/graphic.cs
+++ NeverLand1/NeverLand1/graphic.cs
@@ -21,8 +21,10 @@ namespace NeverLand1
             btm = new Bitmap(box.Width, box.Height);
             g = Graphics.FromImage(btm);  //graphics for a boundary
             box.Image = btm;
+            bm = new Bitmap(10, 10);
 
         }
+        Bitmap bm;
         public void update()
         {
             p = new Pen(Brushes.Aqua);
@@ -30,7 +32,10 @@ namespace NeverLand1
             g.DrawRectangle(p, r);
             p.Brush = Brushes.Black;
             g.DrawEllipse(p, 20, 20, 30, 30);
-            
+
+            Color color = Color.DarkRed;
+            bm.SetPixel(0, 0, color);
+            g.DrawImageUnscaled(bm, 50, 50);
             box.Image = btm;
         }
         public void step_test()
