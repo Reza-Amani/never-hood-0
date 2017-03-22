69b7ccccc85b9d836fc31e06c8ecc599a47bb40e
RezaAm <reza.amani@trutest.co.nz>
2017-03-23 11:53:58 +1300

    added DNA and Gene constructors from cell

--- NeverLand1/NeverLand1/Gene.cs
+++ NeverLand1/NeverLand1/Gene.cs
@@ -11,6 +11,13 @@ namespace NeverLand1
         {
             _mouth, _cholorophyl, _genital_male, _genital_female, _fin, _crawling_leg
         }
+        public Gene(GeneType _name, int _value, int _min, int _max)
+        {   //initial constructor
+            value = _value;
+            min = _min;
+            max = _max;
+            name = _name;
+        }
         public Gene(Gene _gene)
         {   //copy constructor
             value = _gene.value;
@@ -19,7 +26,7 @@ namespace NeverLand1
             name = _gene.name;
         }
         public Gene(Gene _gene1, Gene _gene2)
-        {
+        {   //mutation constructor
             if (_gene1.name != _gene2.name)
                 Globals.soft_error("pairing different-type genes");
             name = _gene1.name;
